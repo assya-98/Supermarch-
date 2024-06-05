b28e290d34f6c0e433bf135efd15bfea5ad0d56c
assya-98 <63973862+assya-98@users.noreply.github.com>
2024-06-05 17:55:38 +0200

    Add files via upload

---
+++ SuperMarket/SuperMarket/Factures.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SuperMarket
+{
+    partial class Factures
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Facturation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Facturation
+            // 
+            this.Facturation.AutoSize = true;
+            this.Facturation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Facturation.Location = new System.Drawing.Point(362, 63);
+            this.Facturation.Name = "Facturation";
+            this.Facturation.Size = new System.Drawing.Size(114, 24);
+            this.Facturation.TabIndex = 0;
+            this.Facturation.Text = "Facturation";
+            // 
+            // Factures
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Facturation);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Factures";
+            this.Text = "Factures";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Facturation;
+    }
+}
